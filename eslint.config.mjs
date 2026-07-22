c6a86c9db1f1e291d15f1f83445b998ed15745a9
Chen Asraf <casraf@pm.me>
2026-07-22 09:58:13 +0300

    build: fix eslint no unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,10 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 export default [
-  ...tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended),
+  eslint.configs.recommended, tseslint.configs.recommended,
   {
     rules: {
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-unused-vars': ['off'],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': [
         'warn',
